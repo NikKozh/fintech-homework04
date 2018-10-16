07ddb82ff5e3c253f880be8b0b35b3f301a8a75c
zitt <NKtargeting@yandex.ru>
2018-10-16 22:16:51 +0500

    Implement size, max, depth and map functions

--- src/main/scala/fintech/homework04/Tree.scala
+++ src/main/scala/fintech/homework04/Tree.scala
@@ -5,7 +5,6 @@ case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
-  // реализовать функцию fold
   def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = {
     def iter(t: Tree[A]): B = t match {
       case Leaf(value)         => f(value)
@@ -14,19 +13,28 @@ object Tree {
     iter(t)
   }
 
-  // реализовать следующие функции через fold
+  def size[A](t: Tree[A]): Int = fold(t)(_ => 1)(_ + _)
 
-  def size[A](t: Tree[A]): Int = ???
+  def max(t: Tree[Int]): Int = fold(t)(identity[Int])(scala.math.max)
 
-  def max(t: Tree[Int]): Int = ???
+  def depth[A](t: Tree[A]): Int = fold(t)(_ => 1)(_.max(_) + 1) - 1
 
-  def depth[A](t: Tree[A]): Int = ???
-
-  //здесь возможно придется прибегнуть к насильному указанию типа: Leaf(a): Tree[A]
-  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = ???
+  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = {
+    def iter(t: Tree[A]): Tree[B] = t match {
+      case Leaf(value)         => Leaf(f(value))
+      case Branch(left, right) => Branch(iter(left), iter(right))
+    }
+    iter(t)
+  }
 }
 
 object Main extends App {
-  val tree = Branch(Branch(Leaf(1), Leaf("2")), Branch(Leaf(3), Leaf("4")))
-  println(Tree.fold(tree)(_.toString)(_ + _))
+  val tree = Branch(Branch(Leaf(1), Branch(Leaf(4), Leaf(3))), Leaf(3))
+  val tree2 = Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4)))
+  println("fold: " + Tree.fold(tree)(_.toString)(_ + _))
+  println("size: " + Tree.size(tree))
+  println("max: " + Tree.max(tree))
+  println("depth: " + Tree.depth(tree2))
+
+  println(Tree.map(tree2)(_ * 10))
 }
