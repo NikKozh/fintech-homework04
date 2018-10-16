b1b986e3b9cb6c70eae5cedfee9834dfe8f151b5
Nikita Kozhevnikov <NKtargeting@yandex.ru>
2018-10-16 16:26:14 +0500

    Implement fold function

--- src/main/scala/fintech/homework04/Tree.scala
+++ src/main/scala/fintech/homework04/Tree.scala
@@ -6,7 +6,13 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
   // реализовать функцию fold
-  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = ???
+  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = {
+    def iter(t: Tree[A]): B = t match {
+      case Leaf(value)         => f(value)
+      case Branch(left, right) => g(iter(left), iter(right))
+    }
+    iter(t)
+  }
 
   // реализовать следующие функции через fold
 
@@ -18,4 +24,9 @@ object Tree {
 
   //здесь возможно придется прибегнуть к насильному указанию типа: Leaf(a): Tree[A]
   def map[A,B](t: Tree[A])(f: A => B): Tree[B] = ???
+}
+
+object Main extends App {
+  val tree = Branch(Branch(Leaf(1), Leaf("2")), Branch(Leaf(3), Leaf("4")))
+  println(Tree.fold(tree)(_.toString)(_ + _))
 }
