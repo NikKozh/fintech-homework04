8767f84c8ac82940c0be0ee6dd2b61eefec61338
Nikita Kozhevnikov <NKtargeting@yandex.ru>
2018-10-17 11:15:24 +0500

    Implement dataSize method, rework map

--- src/main/scala/fintech/homework04/Tree.scala
+++ src/main/scala/fintech/homework04/Tree.scala
@@ -32,17 +32,14 @@ object Tree {
 
   def size[A](t: Tree[A]): Int = fold(t)(_ => 1)(_ + _ + 1)
 
+  def dataSize[A](t: Tree[A]): Int = fold(t)(_ => 1)(_ + _)
+
   def max(t: Tree[Int]): Int = fold(t)(identity[Int])(scala.math.max)
 
   def depth[A](t: Tree[A]): Int = fold(t)(_ => 1)(_.max(_) + 1) - 1
 
-  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = {
-    def iter(t: Tree[A]): Tree[B] = t match {
-      case Leaf(value)         => Leaf(f(value))
-      case Branch(left, right) => Branch(iter(left), iter(right))
-    }
-    iter(t)
-  }
+  def map[A,B](t: Tree[A])(f: A => B): Tree[B] =
+    fold(t)(value => Leaf(f(value)).asInstanceOf[Tree[B]])((left, right) => Branch(left, right))
 }
 
 object Main extends App {
